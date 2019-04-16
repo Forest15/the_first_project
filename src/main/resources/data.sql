87b44f3d3f0dc5b141ec0af70c9776df2122e343
vcherevko <vcherevko@bellintegrator.com>
2019-04-16 18:27:35 +0300

    Added comments for all the tables fields. Added sequences for all the PK of the tables. The doc_type field name of the user_doc table was changed on the doc_type_id.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,95 +1,95 @@
 /*Here, I insert the data to the organization table.*/
-INSERT INTO tfp.organization ( id, name, full_name, inn, kpp, address, phone, is_active )
-VALUES ( 1, 'Beeline', 'Vimpelcom', '111111111111', '111111111', 'ул. 8-го марта', '84959999990', true ) ;
+INSERT INTO tfp.organization (name, full_name, inn, kpp, address, phone, is_active )
+VALUES ( 'Beeline', 'Vimpelcom', '111111111111', '111111111', 'ул. 8-го марта', '84959999990', true ) ;
 
-INSERT INTO tfp.organization ( id, name, full_name, inn, kpp, address, phone, is_active )
-VALUES ( 2, 'MTS', 'Mobile Telecom Systems', '222222222222', '222222222' ,'ул. МТС, дом 2', '84959999980', true ) ;
+INSERT INTO tfp.organization (name, full_name, inn, kpp, address, phone, is_active )
+VALUES ( 'MTS', 'Mobile Telecom Systems', '222222222222', '222222222' ,'ул. МТС, дом 2', '84959999980', true ) ;
 
 /*Here, I insert the data to the office table.*/
-INSERT INTO tfp.office ( id, org_id, name, address, office_phone, is_active )
-VALUES ( 1, 1, 'BeelineTechOffice', 'ул. Станции Билайн', '84959999991', true ) ;
+INSERT INTO tfp.office ( org_id, name, address, office_phone, is_active )
+VALUES ( 1, 'BeelineTechOffice', 'ул. Станции Билайн', '84959999991', true ) ;
    
-INSERT INTO tfp.office ( id, org_id, name, address, office_phone, is_active )
-VALUES ( 2, 1, 'BeelineSalesOffice', 'ул. Офиса продаж Билайн', '84959999992', true ) ;
+INSERT INTO tfp.office ( org_id, name, address, office_phone, is_active )
+VALUES ( 1, 'BeelineSalesOffice', 'ул. Офиса продаж Билайн', '84959999992', true ) ;
    
-INSERT INTO tfp.office ( id, org_id, name, address, office_phone, is_active )
-VALUES ( 3, 2, 'MTSTechOffice', 'ул. Станции МТС', '84959999981', true ) ;
+INSERT INTO tfp.office ( org_id, name, address, office_phone, is_active )
+VALUES ( 2, 'MTSTechOffice', 'ул. Станции МТС', '84959999981', true ) ;
    
-INSERT INTO tfp.office ( id, org_id, name, address, office_phone, is_active )
-VALUES ( 4, 2, 'MTSSalesOffice', 'ул. Офиса продаж МТС', '84959999982', true ) ;
+INSERT INTO tfp.office ( org_id, name, address, office_phone, is_active )
+VALUES ( 2, 'MTSSalesOffice', 'ул. Офиса продаж МТС', '84959999982', true ) ;
  
 /*Here, I insert the data to the country table.*/
-INSERT INTO tfp.country ( id, code, name )
-VALUES ( 1, '643', 'Российская Федерация' ) ;
+INSERT INTO tfp.country (code, name )
+VALUES ('643', 'Российская Федерация' ) ;
 
-INSERT INTO tfp.country ( id, code, name )
-VALUES ( 2, '276', 'ФРГ' ) ;
+INSERT INTO tfp.country (code, name )
+VALUES ('276', 'ФРГ' ) ;
 
-INSERT INTO tfp.country ( id, code, name )
-VALUES ( 3, '348', 'Венгрия' ) ;
+INSERT INTO tfp.country (code, name )
+VALUES ('348', 'Венгрия' ) ;
 
 /*Here, I insert the data to the user table.*/
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 1, 1, 1, 'Vladimir', 'Igorevich', 'Cherevko', 'admin', '*1', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (1, 1, 'Vladimir', 'Igorevich', 'Cherevko', 'admin', '*1', true ) ;
 
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 2, 2, 1, 'Vasiliy', 'Romanovich', 'Kapitonov', 'developer', '*2', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (2, 1, 'Vasiliy', 'Romanovich', 'Kapitonov', 'developer', '*2', true ) ;
 
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 3, 1, 2, 'Boris', 'Igorevich', 'Andreev', 'manager', '*1', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (1, 2, 'Boris', 'Igorevich', 'Andreev', 'manager', '*1', true ) ;
 
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 4, 2, 2, 'Ivan', 'Vasilievich', 'Volkov', 'specialist', '*2', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (2, 2, 'Ivan', 'Vasilievich', 'Volkov', 'specialist', '*2', true ) ;
 
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 5, 1, 3, 'Petr', 'Semenovich', 'Ivanov', 'admin', '*1', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (1, 3, 'Petr', 'Semenovich', 'Ivanov', 'admin', '*1', true ) ;
 
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 6, 3, 3, 'Igor', 'Aleksandrovich', 'Kulagin', 'developer', '*2', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (3, 3, 'Igor', 'Aleksandrovich', 'Kulagin', 'developer', '*2', true ) ;
 
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 7, 1, 4, 'Andrey', 'Andreevich', 'Fedotkin', 'manager', '*1', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (1, 4, 'Andrey', 'Andreevich', 'Fedotkin', 'manager', '*1', true ) ;
 
-INSERT INTO tfp.user ( id, country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
-VALUES ( 8, 3, 4, 'Semen', 'Petrovich', 'Vasiliev', 'specialist', '*2', true ) ;
+INSERT INTO tfp.user (country_id, office_id, first_name, middle_name, second_name, position, user_phone, is_identified )
+VALUES (3, 4, 'Semen', 'Petrovich', 'Vasiliev', 'specialist', '*2', true ) ;
 
 /*Here, I insert the data to the doc_type table.*/
-INSERT INTO tfp.doc_type ( id, code, name )
-VALUES ( 1, '21', 'Паспорт гражданина РФ' ) ;
+INSERT INTO tfp.doc_type (code, name )
+VALUES ('21', 'Паспорт гражданина РФ' ) ;
 
-INSERT INTO tfp.doc_type ( id, code, name )
-VALUES ( 2, '12', 'Вид на жительство в РФ' ) ;
+INSERT INTO tfp.doc_type (code, name )
+VALUES ('12', 'Вид на жительство в РФ' ) ;
 
-INSERT INTO tfp.doc_type ( id, code, name )
-VALUES ( 3, '10', 'Паспорт иностранного гражданина' ) ;
+INSERT INTO tfp.doc_type (code, name )
+VALUES ('10', 'Паспорт иностранного гражданина' ) ;
 
 /*Here, I insert the data to the user_doc table.*/
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 1, 1, '9009908991', 'State Department #1', '115-551', '11.03.88', true ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 1, 2, '9009908992', 'State Department #2', '115-552', '12.03.88', false ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 2, 2, '9009908993', 'State Department #3', '115-553', '01.05.90', true ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 2, 3, '9009908994', 'State Department #4', '115-554', '02.05.90', false ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 3, 1, '9009908995', 'State Department #5', '115-555', '10.06.90', true ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 4, 1, '9009908996', 'State Department #6', '115-556', '20.06.90', true ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 5, 2, '9009908997', 'State Department #7', '115-557', '20.07.90', true ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 6, 2, '9009908998', 'State Department #8', '115-558', '15.09.90', true ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 7, 3, '9009908999', 'State Department #9', '115-559', '15.10.90', true ) ;
 
-INSERT INTO tfp.user_doc ( user_id, doc_id, ser_num, who_issued, dep_code, doc_date, is_general )
+INSERT INTO tfp.user_doc ( user_id, doc_type_id, ser_num, who_issued, dep_code, doc_date, is_general )
 VALUES ( 8, 3, '9009908990', 'State Department #10', '115-550', '16.10.90', true ) ;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,13 +3,21 @@ CREATE SCHEMA tfp AUTHORIZATION SA ;
 
 /*This table contains basic characteristics of organization.*/
 CREATE TABLE IF NOT EXISTS tfp.organization (
+-- The org id created with sequence
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- The company short name
     name VARCHAR(30) NOT NULL,
+-- The company full name
     full_name VARCHAR(60) NOT NULL,
+-- The company inn
     inn VARCHAR(12) NOT NULL,
+-- The company kpp
     kpp VARCHAR(9) NOT NULL,
+-- The company legal address
     address VARCHAR(50) NOT NULL,
+-- The company phone
     phone VARCHAR(15) NOT NULL,
+-- Is the company active?
     is_active BOOLEAN NOT NULL
 );
 
@@ -17,11 +25,17 @@ CREATE INDEX ix_organization_id ON tfp.organization (id);
 
 /*This table contains basic characteristics of office in organization. The table has m2o relationship with the organization table.*/
 CREATE TABLE IF NOT EXISTS tfp.office (
+-- The office id created with sequence
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- The FK references on the organization table
     org_id INTEGER,
+-- The office name
     name VARCHAR(50) NOT NULL,
+-- The office address
     address VARCHAR(50) NOT NULL,
+-- The office phone
     office_phone VARCHAR(15) NOT NULL,
+-- Is the office active?
     is_active BOOLEAN NOT NULL,
     FOREIGN KEY (org_id) REFERENCES tfp.organization(id)
 );
@@ -31,8 +45,11 @@ CREATE INDEX ix_office_org_id ON tfp.office (org_id);
 
 /*The country table, contains id, country code, country name. Relationship with the user table is o2m*/
 CREATE TABLE IF NOT EXISTS tfp.country (
+-- The country id created with sequence
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- The State country code
     code VARCHAR(3) NOT NULL,
+-- The country name
     name VARCHAR(60) NOT NULL
 );
 
@@ -40,14 +57,23 @@ CREATE INDEX ix_country_id ON tfp.country (id);
 
 /*This table contains basic characteristic of user working in office. The Table has m2o relationship with the office table, has m2o relationship with the country table and has m2m relationship with doc_type table by the user_doc junction table.*/
 CREATE TABLE IF NOT EXISTS tfp.user (
+-- The user id created with sequence
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- The FK references on the country table
     country_id INTEGER,
+-- The FK references on the office table
     office_id INTEGER,
+-- The first name of user
     first_name VARCHAR(50) NOT NULL,
+-- The middle name of user
     middle_name VARCHAR(50) NOT NULL,
+-- The second name of user
     second_name VARCHAR(50) NOT NULL,
+-- The position of user
     position VARCHAR(20) NOT NULL,
+-- The user's phone
     user_phone VARCHAR(15) NOT NULL,
+-- Is the user identified?
     is_identified BOOLEAN NOT NULL,
     FOREIGN KEY (country_id) REFERENCES tfp.country(id),
     FOREIGN KEY (office_id) REFERENCES tfp.office(id)
@@ -59,8 +85,11 @@ CREATE INDEX ix_user_office_id ON tfp.user (office_id);
 
 /*The document type table, contains id, document code, document name. Relationship with the user table is m2m by using the user_doc table*/
 CREATE TABLE IF NOT EXISTS tfp.doc_type(
+-- The doc_type id created with sequence
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- The State Document code
     code VARCHAR(2) NOT NULL,
+-- The State Document name
     name VARCHAR(60) NOT NULL
 );
 
@@ -68,12 +97,19 @@ CREATE INDEX ix_doc_type_id ON tfp.doc_type (id);
 
 /*The user_doc table, contains the user table PK, the doc_type table PK, document date, is_general field (Is this doc general then 1 else 0). This is the junction table for m2m relationship the user and the doc_type tables.*/
 CREATE TABLE IF NOT EXISTS tfp.user_doc (
+-- The first part of complex PK of the user_doc table. It is reference to the user table PK.
     user_id INTEGER NOT NULL REFERENCES tfp.user(id),
-    doc_id INTEGER NOT NULL REFERENCES tfp.doc_type(id),
+-- The second part of complex PK of the user_doc table. It is reference to the doc_type table PK.
+    doc_type_id INTEGER NOT NULL REFERENCES tfp.doc_type(id),
+-- Series and Number of the user document
     ser_num VARCHAR(10) NOT NULL,
+-- Name of State Department which issued the document
     who_issued VARCHAR(50) NOT NULL,
+-- Code of State Department whick issued the document
     dep_code VARCHAR(7),
+-- The document issue date
     doc_date VARCHAR(15) NOT NULL,
+-- Is the document general for this user. Only one of the user documents (for particular user) may be general.
     is_general BOOLEAN NOT NULL,
-    PRIMARY KEY (user_id, doc_id)
+    PRIMARY KEY (user_id, doc_type_id)
 );
